34c5a2dea45956ebbe1dbfc1332e5cff10b30ef2
axelallain <axel.allain.a@gmail.com>
2021-05-23 07:04:53 +0200

    BandDao repository fixed. Band model was the cause.

--- src/main/kotlin/fr/axelallain/ktest/Models/Band.kt
+++ src/main/kotlin/fr/axelallain/ktest/Models/Band.kt
@@ -2,15 +2,12 @@ package fr.axelallain.ktest.Models
 
 import javax.persistence.*
 
-class Band(s: String) {
+@Entity
+data class Band(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Int,
 
-    @Entity
-    data class Band(
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        val id: Int,
-
-        @Column(nullable = false)
-        val name: String
-    )
-}
+    @Column(nullable = false)
+    val name: String
+)
